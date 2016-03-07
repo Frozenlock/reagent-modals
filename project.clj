2392d85330bd6da5df171d38cd6f74c0962f683c
Frozenlock <frozenlock@gmail.com>
2016-03-07 00:12:59 -0500

    up version

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject org.clojars.frozenlock/reagent-modals "0.2.3"
+(defproject org.clojars.frozenlock/reagent-modals "0.2.4"
   :description "Bootstrap modal components for Reagent!"
-  :url "http://example.com/FIXME"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :clojurescript? true
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [reagent "0.4.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [reagent "0.5.1"]]
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript "0.0-2371" :scope "provided"]]
               :plugins [[lein-cljsbuild "1.0.3"]]}})
